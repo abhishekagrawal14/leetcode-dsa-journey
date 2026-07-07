72225ce80b5002f61d248c1e0e79c12f5caebb19
Abhishek Kumar Agrawal <godabhi813@gmail.com>
2026-07-07 19:41:38 +0530

    Time: 0 ms (100%), Space: 15.8 MB (6.71%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int b = 0 ;
+        int count = 0 ;
+        for(int i = n-1 ; i>0 ;i--){
+            if(nums[i]>nums[i-1]){
+                b = i-1 ;
+                count++;
+                break;
+            }  
+        }
+        if(count == 0 ) {
+            sort(nums.begin(),nums.end());
+            return;
+        }
+
+        int mini = b+1 ;
+        for(int j = b+2 ; j<n ; j++){
+            if(nums[j] > nums[b] && nums[j] <= nums[mini]){
+                 mini = j;
+             }
+        }
+        swap(nums[b],nums[mini]);
+        
+        reverse(nums.begin() + b + 1, nums.end());
+        
+    }
+};
